d53888c1cd2bc0c8f8c11caff8a145fab4f7424d
Jean-Baptiste Lorteau <jb.lorteau@augmentedev.com>
2019-06-04 19:14:13 +0200

    Initial commit

---
+++ apps/main.cpp
@@ -0,0 +1,35 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main() {
+
+    if(!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLViewer", nullptr, nullptr);
+
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
---
+++ include/gle/gle.h
@@ -0,0 +1,12 @@
+#ifndef GLE_GLE_H
+#define GLE_GLE_H
+
+#include <string>
+
+namespace {
+    inline std::string version() {
+        return "0.0.1";
+    }
+}
+
+#endif
